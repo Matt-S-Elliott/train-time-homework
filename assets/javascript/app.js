732ed38bcf76432b09d6adf74b0e4226863d4376
Matthew Elliott <mse951993@gmail.com>
2019-03-07 16:21:39 -0800

    HTML setup and styling complete

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyDlFMpPTHMd-cOXKEUFZuu5GbB4G7tuzmo",
+    authDomain: "train-times-homework-cbc.firebaseapp.com",
+    databaseURL: "https://train-times-homework-cbc.firebaseio.com",
+    projectId: "train-times-homework-cbc",
+    storageBucket: "",
+    messagingSenderId: "232612117818"
+  };
+  firebase.initializeApp(config);
+
+  
